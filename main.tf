2013b6595cb3ccefa7403e16b61bb92d555a1262
Azhan Ahmad Shakeel Ahmad <azhanahmad9300@gmail.com>
2025-10-22 22:25:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  count         = 2
-  ami           = "ami-0b09ffb6d8b58ca91"
-  instance_type = "t3.micro"
- 
-  tags = {
-    Name = " hcp-server"
-  }
+  count         = 5
+  ami           = "ami-0341d95f75f311023"
+  instance_type = "t3.small"
 }
